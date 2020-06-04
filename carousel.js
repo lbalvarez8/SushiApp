c042a828e371b98d641fbe18e5c2a4597a5a19b6
Lena Zerlav <lenaz@Lenas-MBP.attlocal.net>
2020-06-04 13:01:50 -0700

    first commit

---
+++ carousel.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".carousel").slick({
+    autoplay: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    autoplaySpeed: 3000,
+    speed: 300,
+    fade: true,
+    cssEase: "linear",
+    dots: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
+});
